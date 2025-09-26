a7441a877a268a7dca6a2b3db3f92adb911385d0
NGOC DAO <lengocdao0103@gmail.com>
2025-09-26 12:04:03 +0700

    fix bug

--- src/pages/admin/Dashboard.vue
+++ src/pages/admin/Dashboard.vue
@@ -1,1 +1,3 @@
-<template></template>
+<template>
+    <h1>Hi Dashboard</h1>
+</template>
--- src/pages/admin/Layout.vue
+++ src/pages/admin/Layout.vue
@@ -1,3 +1,7 @@
+<script setup>
+import Dashboard from './Dashboard.vue'
+</script>
 <template>
     <h1>hi admin</h1>
+    <Dashboard></Dashboard>
 </template>
